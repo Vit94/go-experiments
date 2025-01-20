b5093e6550c39f784f230acb526e6b48dcd1198a
Vit94 <santata600@gmail.com>
2025-01-20 17:30:10 +0500

    Особенности каналов

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vit94/go-experiments
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Vit94/go-experiments/channels"
+
+func main() {
+	channels.BufferedChannel()
+	channels.UnbufferedChannel()
+}
